ad85c20865e9c6b63a895f55187fa16d37d5e6b8
Titus Nguyen <titusstar007@gmail.com>
2023-05-14 15:56:38 -0700

    Added cmd line screen switch

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import glob
+import sys
 from textual.app import App
 from screens.WelcomeScreen import WelcomeScreen
 from screens.LoadingScreen import LoadingScreen
+
+from textual.screen import Screen
+from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Header, Label, Input, MarkdownViewer, DataTable 
+
+class MainScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header()
+
+
+
+
+
+
+
 
 
 class TheLivingTerminal(App):
@@ -9,17 +26,22 @@ class TheLivingTerminal(App):
      
     CSS_PATH = list(glob.glob("./css/*.css"))
     SCREENS = {"welcome": WelcomeScreen(),
-                "loading": LoadingScreen()}
+                "loading": LoadingScreen(),
+                "main": MainScreen()}
    
     def on_loading_screen_progress_bar_complete(self) -> None:
         self.pop_screen()
+        self.push_screen("main")
 
     def on_login_form_authenticated(self) -> None:
         self.pop_screen()
         self.push_screen("loading")
 
     def on_mount(self) -> None:
-        self.push_screen("loading")
+        if __name__ == "__main__" and len(sys.argv) > 1:
+            self.push_screen(sys.argv[1])
+        else: 
+            self.push_screen("welcome")
 
 
 if __name__ == "__main__":
